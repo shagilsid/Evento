a40e685e79206413401516dee11b5db2ea1c7647
shagilsid <shagilsid@gmail.com>
2016-03-17 19:58:11 +0530

    Added files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1140px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 320px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 250);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+	//slider init
+	$("#slides").responsiveSlides({
+		speed: 700,     //Speed of the transition
+		timeout: 3500,  //Time between slide transitions
+		pager: true     //Show the pagination
+	});
+	
+
+	//wow.js init
+	wow = new WOW(
+	    {
+		  animateClass: 'animated',
+		  mobile: false,
+		  offset: 100
+		}
+	);
+	wow.init();
+		// Scrolly.
+			$('.scrolly').scrolly();
+
+	});
+
+})(jQuery);
